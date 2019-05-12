a924af30690d5688c520123d6fa2c6d41ed731d2
InquisitiveMindHasToKnow <omarraymond@pursuit.org>
2019-05-12 11:49:01 -0400

    Login and user registration pages made using shared prefs to save info

---
+++ app/src/main/java/com/example/cupidshuffle/LoginActivity.java
@@ -0,0 +1,96 @@
+package com.example.cupidshuffle;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.SystemClock;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private static final String SHARED_PREFS_KEY = "sharedPrefsTesting";
+    private long lastButtonClickTime = 0;
+    private TextView loginScreenTitleTextView;
+    private TextView loginOrRegisterPrompt;
+    private EditText usernameEditText;
+    private EditText passwordEditText;
+    private CheckBox rememberMeCheckBox;
+    private Button loginButton;
+    private Button registerButton;
+    private SharedPreferences loginSharedPreferences;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        loginScreenTitleTextView = findViewById(R.id.login_screen_title_textview);
+        loginOrRegisterPrompt = findViewById(R.id.sign_in_prompt_textview);
+        usernameEditText = findViewById(R.id.user_name_edittext);
+        passwordEditText = findViewById(R.id.user_password_edittext);
+        rememberMeCheckBox = findViewById(R.id.remember_me_checkbox);
+        loginButton = findViewById(R.id.login_button);
+        registerButton = findViewById(R.id.register_button);
+
+        loginSharedPreferences = getApplicationContext().getSharedPreferences(SHARED_PREFS_KEY, MODE_PRIVATE);
+
+        if (loginSharedPreferences.getBoolean("isChecked", false)) {
+            usernameEditText.setText(loginSharedPreferences.getString("username", null));
+            passwordEditText.setText(loginSharedPreferences.getString("password", null));
+            rememberMeCheckBox.setChecked(loginSharedPreferences.getBoolean("isChecked", false));
+        }
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (SystemClock.elapsedRealtime() - lastButtonClickTime < 3000) {
+                    return;
+                }
+                lastButtonClickTime = SystemClock.elapsedRealtime();
+
+                SharedPreferences.Editor editor = loginSharedPreferences.edit();
+                if (rememberMeCheckBox.isChecked()) {
+                    editor.putString("username", usernameEditText.getText().toString());
+                    editor.putString("password", passwordEditText.getText().toString());
+                    editor.putBoolean("isChecked", rememberMeCheckBox.isChecked());
+                    editor.commit();
+                } else {
+                    editor.putBoolean("isChecked", rememberMeCheckBox.isChecked());
+                    editor.commit();
+                }
+                String checkUser = "user" + usernameEditText.getText().toString();
+                String checkPassword = "password" + usernameEditText.getText().toString();
+
+                if (usernameEditText.getText().toString().equalsIgnoreCase(loginSharedPreferences.getString(checkUser, null))
+                        && passwordEditText.getText().toString().equals(loginSharedPreferences.getString(checkPassword, null))) {
+                    Intent loginIntent = new Intent(LoginActivity.this, MainActivity.class);
+                    loginIntent.putExtra("currentUser", usernameEditText.getText().toString());
+                    LoginActivity.this.startActivity(loginIntent);
+                } else {
+                    Toast.makeText(LoginActivity.this.getApplicationContext(), "Username or password invalid!", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (SystemClock.elapsedRealtime() - lastButtonClickTime < 3000) {
+                    return;
+                }
+                lastButtonClickTime = SystemClock.elapsedRealtime();
+                Intent registerIntent = new Intent(LoginActivity.this, UserRegistrationActivity.class);
+                registerIntent.putExtra("testKey", SHARED_PREFS_KEY);
+                LoginActivity.this.startActivity(registerIntent);
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/cupidshuffle/UserRegistrationActivity.java
@@ -0,0 +1,63 @@
+package com.example.cupidshuffle;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.SystemClock;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class UserRegistrationActivity extends AppCompatActivity {
+
+        private SharedPreferences registrationSharedPrefs;
+        private EditText registrationUserName;
+        private EditText registrationPassword;
+        private EditText confirmPassword;
+        private Intent registrationIntent;
+        private long lastButtonClickTime = 0;
+        private Button registerButton;
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_user_registration);
+
+            registrationUserName = findViewById(R.id.user_registration_name_edittext);
+            registrationPassword = findViewById(R.id.user_registration_password_edittext);
+            confirmPassword = findViewById(R.id.user_registration_confirm_password_edittext);
+            registerButton = findViewById(R.id.user_registration_submit_button);
+
+            registrationIntent = getIntent();
+            registrationSharedPrefs = getApplicationContext().getSharedPreferences(registrationIntent.getStringExtra("testKey"), MODE_PRIVATE);
+
+            registerButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if (SystemClock.elapsedRealtime() - lastButtonClickTime < 3000) {
+                        return;
+                    }
+                    lastButtonClickTime = SystemClock.elapsedRealtime();
+
+                    SharedPreferences.Editor editor = registrationSharedPrefs.edit();
+
+
+                    if (registrationUserName.getText() != null && registrationPassword.getText() != null && confirmPassword.getText() != null &&
+                            registrationPassword.getText().toString().equals(confirmPassword.getText().toString()
+                            )) {
+                        editor.putString("user" + registrationUserName.getText().toString(), registrationUserName.getText().toString());
+                        editor.putString("password" + registrationUserName.getText().toString(), registrationPassword.getText().toString());
+                        editor.apply();
+                        UserRegistrationActivity.this.finish();
+                    }
+
+                    if (!registrationPassword.getText().toString().equals(confirmPassword.getText().toString())) {
+                        Toast.makeText(UserRegistrationActivity.this.getApplicationContext(), "Try again. Both passwords have to match", Toast.LENGTH_LONG).show();
+
+                    }
+                }
+            });
+        }
+    }

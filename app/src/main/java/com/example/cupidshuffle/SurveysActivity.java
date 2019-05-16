15f6932c29789652cc56581906fa35cfdea701c5
pillmatictm <pilinchong@pursuit.org>
2019-05-15 20:23:27 -0400

    starts working on surveys

--- app/src/main/java/com/example/cupidshuffle/SurveysActivity.java
+++ app/src/main/java/com/example/cupidshuffle/SurveysActivity.java
@@ -1,4 +1,4 @@
-package com.example.cupidshuffle.survey;
+package com.example.cupidshuffle;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
---
+++ app/src/main/java/com/example/cupidshuffle/survey/Destination.java
@@ -0,0 +1,5 @@
+package com.example.cupidshuffle.survey;
+
+public enum Destination {
+
+}

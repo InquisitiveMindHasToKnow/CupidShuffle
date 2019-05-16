a0c6568dff0bac7d22ca3d71f24898c4ad70228a
InquisitiveMindHasToKnow <omarraymond@pursuit.org>
2019-05-15 20:59:12 -0400

    main user profile made with image, main user's info, ways for them to communicate and navigate the app and people they may be interested in.

---
+++ app/src/main/java/com/example/cupidshuffle/activities/MainUserProfile.java
@@ -0,0 +1,15 @@
+package com.example.cupidshuffle.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.cupidshuffle.R;
+
+public class MainUserProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_user_profile);
+    }
+}

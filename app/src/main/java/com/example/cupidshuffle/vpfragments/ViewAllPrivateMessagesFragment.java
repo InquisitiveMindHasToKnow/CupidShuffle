bc2101345b8a018bbb9fc68c0edd1adff9d51b2a
pillmatictm <pilinchong@pursuit.org>
2019-05-25 15:11:19 -0400

    pulled and merged from master

--- app/src/main/java/com/example/cupidshuffle/vpfragments/ViewAllPrivateMessagesFragment.java
+++ app/src/main/java/com/example/cupidshuffle/vpfragments/ViewAllPrivateMessagesFragment.java
@@ -1,10 +1,8 @@
 package com.example.cupidshuffle.vpfragments;
 
 
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
@@ -17,10 +15,6 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.cupidshuffle.R;
-import com.example.cupidshuffle.RetrofitSingleton;
-import com.example.cupidshuffle.activities.FragmentHolder;
-import com.example.cupidshuffle.activities.ShuffleTheLoveBirdsActivity;
-import com.example.cupidshuffle.activities.ViewUserProfilesActivity;
 import com.example.cupidshuffle.model.PrivateMessages;
 import com.example.cupidshuffle.model.PrivateMessagesAPI;
 import com.example.cupidshuffle.rv.PrivateMessagesAdapter;

3ae382666ea1019e1fe8e1808317eec153bbc42c
InquisitiveMindHasToKnow <omarraymond@pursuit.org>
2019-06-02 12:41:21 -0400

    allowed users to send reservation details through email

--- app/src/main/java/com/example/cupidshuffle/activities/ShowReservationActivity.java
+++ app/src/main/java/com/example/cupidshuffle/activities/ShowReservationActivity.java
@@ -65,13 +65,13 @@ public class ShowReservationActivity extends AppCompatActivity {
             public void onClick(View v) {
 
 
-                Intent i = new Intent(Intent.ACTION_SEND);
-                i.setType("message/rfc822");
-                i.putExtra(Intent.EXTRA_EMAIL  , new String[]{"recipient@example.com"});
-                i.putExtra(Intent.EXTRA_SUBJECT, "subject of email");
-                i.putExtra(Intent.EXTRA_TEXT   , "body of email");
+                Intent emailIntent = new Intent(Intent.ACTION_SEND);
+                emailIntent.setType("message/rfc822");
+                emailIntent.putExtra(Intent.EXTRA_EMAIL  , new String[]{"recipient@example.com"});
+                emailIntent.putExtra(Intent.EXTRA_SUBJECT, "subject of email");
+                emailIntent.putExtra(Intent.EXTRA_TEXT   , "body of email");
                 try {
-                    startActivity(Intent.createChooser(i, "Send mail..."));
+                    startActivity(Intent.createChooser(emailIntent, "Send mail..."));
                 } catch (android.content.ActivityNotFoundException ex) {
                     Toast.makeText(ShowReservationActivity.this, "There are no email clients installed.", Toast.LENGTH_SHORT).show();
                 }
